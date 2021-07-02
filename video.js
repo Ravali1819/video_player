9fbe372edda1c946118c2b83eeaf76ab71fd4d6d
Ravali <81100442+Ravali1819@users.noreply.github.com>
2021-07-02 21:42:31 +0530

    Update video.js

--- video.js
+++ video.js
@@ -1,4 +1,57 @@
 $(document).ready(function () {
+  var playlistData = [
+    {
+      id: "1",
+      title: "Croissants | Flour and Stone",
+      thumbnail: "https://i.vimeocdn.com/video/600595198_390x220.webp",
+    },
+    {
+      id: "2",
+      title: "Perfect Mashed Potatoes and Gravy",
+      thumbnail: "https://i.vimeocdn.com/video/604150056_390x220.webp",
+    },
+    {
+      id: "3",
+      title: "The Heart of Soba",
+      thumbnail: "https://i.vimeocdn.com/video/726986673_390x220.webp",
+    },
+    {
+      id: "4",
+      title: "Grilled Cheese 9 Ways",
+      thumbnail: "https://i.vimeocdn.com/video/570486309_390x220.webp",
+    },
+    {
+      id: "5",
+      title: "Pineapple Cheesecake",
+      thumbnail: "https://i.vimeocdn.com/video/602705517_390x220.webp",
+    },
+    {
+      id: "6",
+      title: "Lemony Chicken Noodle Soup",
+      thumbnail: "https://i.vimeocdn.com/video/537261616_390x220.jpg",
+    },
+    {
+      id: "7",
+      title: "Pumpkin Roll",
+      thumbnail: "https://i.vimeocdn.com/video/608805594_590x332.jpg",
+    },
+    {
+      id: "8",
+      title: "How to Brine a Turkey",
+      thumbnail: "https://i.vimeocdn.com/video/601730519_590x332.jpg",
+    },
+    {
+      id: "9",
+      title: "Stop Motion Dry-Brined Turkey Recipe",
+      thumbnail: "https://i.vimeocdn.com/video/456852083_590x332.jpg",
+    },
+    {
+      id: "10",
+      title: "Butternut Squash Ravioli",
+      thumbnail: "https://i.vimeocdn.com/video/600328152_590x332.jpg",
+    },
+  ];
+
   function createPlaylistCard(obj, pos) {
     var mainDiv = document.createElement("div");
     mainDiv.id = "card" + obj.id;
@@ -19,6 +72,7 @@ $(document).ready(function () {
   }
 
   function updateLeftVideo(id) {
+    console.log(id);
     $.get(
       "https://5d76bf96515d1a0014085cf9.mockapi.io/video/" + id,
       function (videoData) {
@@ -40,23 +94,17 @@ $(document).ready(function () {
     );
   }
 
-  $.get(
-    "https://5d76bf96515d1a0014085cf9.mockapi.io/playlist",
-    function (response) {
-      // console.log(response);
+  for (let i = 1; i <= playlistData.length; i++) {
+    console.log(playlistData.length);
+    var data = createPlaylistCard(playlistData[i - 1]);
 
-      for (let i = 0; i < response.length; i++) {
-        var data = createPlaylistCard(response[i]);
-
-        $("#playlist-wrapper").append(data);
-        if (i == 0) {
-          $("#card" + 1).addClass("active-card");
-        }
-        // console.log(data);
-        $("#card" + i).click(function () {
-          updateLeftVideo(i);
-        });
-      }
+    $("#playlist-wrapper").append(data);
+    if (i == 1) {
+      $("#card" + 1).addClass("active-card");
     }
-  );
+    // console.log(data);
+    $("#card" + i).click(function () {
+      updateLeftVideo(i);
+    });
+  }
 });
